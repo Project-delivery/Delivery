6de4eceee2948d3d70fab2d33ccfe1a201561c7b
ACBITE <bajtasovrinat@gmail.com>
2023-04-07 11:42:16 +0300

    Added simple authentication

--- Delivery/Controllers/UserController.cs
+++ Delivery/Controllers/UserController.cs
@@ -20,4 +20,18 @@ public class UserController : Controller
         var response = await _userService.GetUserByName("admin");
         return View(new List<User> () { response.Data });
     }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> ProfilePage(User User)
+    {
+        var _user = await _userService.GetUserByName(User.Name);
+        if (_user.Data==null) return RedirectToAction("Login", "User");
+        if (_user.Data.Password ==null) return RedirectToAction("Login", "User");
+        if (_user.Data.Password != User.Password) return RedirectToAction("Login", "User");
+        return View(User);
+    }
 }
