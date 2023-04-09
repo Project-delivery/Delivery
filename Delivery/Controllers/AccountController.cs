e773996a317cb1429258ae1b5942db6c99c68aee
ACBITE <bajtasovrinat@gmail.com>
2023-04-09 15:27:45 +0300

    Перенес все в Account контроллер

--- Delivery/Controllers/AccountController.cs
+++ Delivery/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using Delivery.Domain.Entity;
 using Delivery.Domain.ViewModel.Account;
 using Delivery.Service.Implementation;
 using Microsoft.AspNetCore.Authentication;
@@ -19,10 +20,25 @@ public class AccountController : Controller
             var response = await AccountService.Register(model);
             if (response.StatusCode == Domain.Enum.StatusCode.OK)
             {
-                return RedirectToAction("Login", "User");
+                return RedirectToAction("Login", "Account");
             }
         }
 
         return RedirectToAction("Register", "Account");
     }
+    
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> ProfilePage(User User)
+    {
+        var _user = await UserService.GetUserByName(User.Name);
+        if (_user.Data==null) return RedirectToAction("Login", "Account");
+        if (_user.Data.Password ==null) return RedirectToAction("Login", "Account");
+        if (_user.Data.Password != User.Password) return RedirectToAction("Login", "Account");
+        return View(_user.Data);
+    }
+
 }
