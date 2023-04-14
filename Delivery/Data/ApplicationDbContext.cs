7c3832856a004e80f23673b5b6450f930a94a1f4
Alkogolek228 <alkhovik.danila@gmail.com>
2023-04-14 23:12:20 +0300

    added admin controller

---
+++ Delivery/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Delivery.Entities;
+
+namespace Delivery.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+       
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Delivery/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Delivery.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Login { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+    }
+}
