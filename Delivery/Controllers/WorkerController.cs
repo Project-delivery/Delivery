e69b58581feb3ad14d9ff01d26640e29d16d28d6
Alkogolek228 <alkhovik.danila@gmail.com>
2023-04-15 15:36:27 +0300

    added workercontroller and protoaddress classes

---
+++ Delivery/Controllers/WorkerController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Delivery.Models;
+using Delivery.Data;
+using Delivery.Entities;
+
+namespace Delivery.Controllers
+{
+    public class WorkerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WorkerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Worker
+        public async Task<IActionResult> Index()
+        {
+            var addresses = await _context.Addresses.ToListAsync();
+            return View(addresses);
+        }
+
+        // GET: Worker/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Worker/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Street,City,State,ZipCode")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(address);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(address);
+        }
+
+        // GET: Worker/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var address = await _context.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Worker/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Street,City,State,ZipCode")] Address address)
+        {
+            if (id != address.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(address);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddressExists(address.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(address);
+        }
+
+        private bool AddressExists(int id)
+        {
+            return _context.Addresses.Any(e => e.Id == id);
+        }
+
+    }
+}
--- Delivery/Data/ApplicationDbContext.cs
+++ Delivery/Data/ApplicationDbContext.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Delivery.Entities;
+using Delivery.Domain.Entity;
+using System.Net;
+using Delivery.Models;
 
 namespace Delivery.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-       
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Добавить конфигурацию моделей
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new AddressConfiguration());
         }
     }
 }
