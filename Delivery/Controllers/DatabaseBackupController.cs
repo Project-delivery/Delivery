e6702c1ab5416b3e3ad6dd7c4a577d9b9f100e82
Alkogolek228 <alkhovik.danila@gmail.com>
2023-05-23 22:19:58 +0300

    database backup controller added

--- Delivery/Controllers/DatabaseBackupController.cs
+++ Delivery/Controllers/DatabaseBackupController.cs
@@ -1,6 +1,26 @@
-﻿namespace Delivery.Controllers
+﻿using Delivery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delivery.Controllers
 {
-    public class DatabaseBackupController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BackupController : ControllerBase
     {
+        private readonly DatabaseBackup databaseBackup;
+
+        public BackupController()
+        {
+            string connectionString = "сюда connection_string";
+            databaseBackup = new DatabaseBackup(connectionString);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBackup()
+        {
+            string backupFilePath = "сюда куда сохранять";
+            databaseBackup.BackupDatabase(backupFilePath);
+            return Ok("Database backup created successfully.");
+        }
     }
 }
